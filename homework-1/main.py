4243933395775ec70d61fd5fd138ab3242a7398b
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-05-21 19:19:52 -0400

    add changes

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,13 +1,15 @@
 from src.channel import Channel
 
+dictionary = {'kind': 'youtube#channelListResponse', 'etag': 'pYjb0XVs9dq2_Xawj-1sjUFZrM4', 'pageInfo': {'totalResults': 1, 'resultsPerPage': 5}, 'items': [{'kind': 'youtube#channel', 'etag': 'm1vtI03TiruCANlqXSqpJL4qMI4', 'id': 'UC9XJvt8OT-9_8QHDBdqocaw', 'snippet': {'title': 'Жекич Дубровский', 'description': 'Основной канал сообщества "Дубровский Синдикат"\n\nДУБРОВСКИЙ СИНДИКАТ - крупнейшее творческое объединение, основанное в 2017 году, включающее в себя десятки популярных каналов на YouTube и других соцсетях, собственное рекламное агентство, автосалоны и сервисы проката в разных городах мира, а так же ресурсы в\xa0сфере\xa0видеоигр.\n\nПо вопросам сотрудничества, спонсорства или рекламы, писать на почту dubrovskiysyndicate@mail.ru', 'customUrl': '@dubrovskiy-syndicate', 'publishedAt': '2013-03-06T02:05:58Z', 'thumbnails': {'default': {'url': 'https://yt3.ggpht.com/ytc/AGIKgqMt5pB_B63OOWd43dY5zGZAQSNczIi4rdM2Rd-DXQ=s88-c-k-c0x00ffffff-no-rj', 'width': 88, 'height': 88}, 'medium': {'url': 'https://yt3.ggpht.com/ytc/AGIKgqMt5pB_B63OOWd43dY5zGZAQSNczIi4rdM2Rd-DXQ=s240-c-k-c0x00ffffff-no-rj', 'width': 240, 'height': 240}, 'high': {'url': 'https://yt3.ggpht.com/ytc/AGIKgqMt5pB_B63OOWd43dY5zGZAQSNczIi4rdM2Rd-DXQ=s800-c-k-c0x00ffffff-no-rj', 'width': 800, 'height': 800}}, 'localized': {'title': 'Жекич Дубровский', 'description': 'Основной канал сообщества "Дубровский Синдикат"\n\nДУБРОВСКИЙ СИНДИКАТ - крупнейшее творческое объединение, основанное в 2017 году, включающее в себя десятки популярных каналов на YouTube и других соцсетях, собственное рекламное агентство, автосалоны и сервисы проката в разных городах мира, а так же ресурсы в\xa0сфере\xa0видеоигр.\n\nПо вопросам сотрудничества, спонсорства или рекламы, писать на почту dubrovskiysyndicate@mail.ru'}, 'country': 'RU'}, 'statistics': {'viewCount': '767257944', 'subscriberCount': '4640000', 'hiddenSubscriberCount': False, 'videoCount': '360'}}]}
+
 if __name__ == '__main__':
-    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
-    moscowpython.print_info()
+    dubrovskiy = Channel('UC9XJvt8OT-9_8QHDBdqocaw')
+    dubrovskiy.print_info(dictionary)
 
     """
-{
+   {
   "kind": "youtube#channelListResponse",
-  "etag": "uAdmwT0aDhY9LmAzJzIafD6ATRw",
+  "etag": "pYjb0XVs9dq2_Xawj-1sjUFZrM4",
   "pageInfo": {
     "totalResults": 1,
     "resultsPerPage": 5
@@ -15,44 +17,47 @@ if __name__ == '__main__':
   "items": [
     {
       "kind": "youtube#channel",
-      "etag": "cPh7A8SKcZxxs_UPCiBaXP1wNDk",
-      "id": "UC-OVMPlMA3-YCIeg4z5z23A",
+      "etag": "m1vtI03TiruCANlqXSqpJL4qMI4",
+      "id": "UC9XJvt8OT-9_8QHDBdqocaw",
       "snippet": {
-        "title": "MoscowPython",
-        "description": "Видеозаписи со встреч питонистов и джангистов в Москве и не только. :)\nПрисоединяйтесь: https://www.facebook.com/groups/MoscowDjango! :)",
-        "customUrl": "@moscowdjangoru",
-        "publishedAt": "2012-07-13T09:48:44Z",
+        "title": "Жекич Дубровский",
+        "description": "Основной канал сообщества \"Дубровский Синдикат\"\n\nДУБРОВСКИЙ СИНДИКАТ - крупнейшее творческое объединение, основанное в 2017 году, включающее в себя десятки популярных каналов на YouTube и других соцсетях, собственное рекламное агентство, автосалоны и сервисы проката в разных городах мира, а так же ресурсы в сфере видеоигр.\n\nПо вопросам сотрудничества, спонсорства или рекламы, писать на почту dubrovskiysyndicate@mail.ru",
+        "customUrl": "@dubrovskiy-syndicate",
+        "publishedAt": "2013-03-06T02:05:58Z",
         "thumbnails": {
           "default": {
-            "url": "https://yt3.ggpht.com/ytc/AGIKgqNv2rZ6mOSuXvJLYhmTc0nd-LtI5RiDtsEBpguJXA=s88-c-k-c0x00ffffff-no-rj",
+            "url": "https://yt3.ggpht.com/ytc/AGIKgqMt5pB_B63OOWd43dY5zGZAQSNczIi4rdM2Rd-DXQ=s88-c-k-c0x00ffffff-no-rj",
             "width": 88,
             "height": 88
           },
           "medium": {
-            "url": "https://yt3.ggpht.com/ytc/AGIKgqNv2rZ6mOSuXvJLYhmTc0nd-LtI5RiDtsEBpguJXA=s240-c-k-c0x00ffffff-no-rj",
+            "url": "https://yt3.ggpht.com/ytc/AGIKgqMt5pB_B63OOWd43dY5zGZAQSNczIi4rdM2Rd-DXQ=s240-c-k-c0x00ffffff-no-rj",
             "width": 240,
             "height": 240
           },
           "high": {
-            "url": "https://yt3.ggpht.com/ytc/AGIKgqNv2rZ6mOSuXvJLYhmTc0nd-LtI5RiDtsEBpguJXA=s800-c-k-c0x00ffffff-no-rj",
+            "url": "https://yt3.ggpht.com/ytc/AGIKgqMt5pB_B63OOWd43dY5zGZAQSNczIi4rdM2Rd-DXQ=s800-c-k-c0x00ffffff-no-rj",
             "width": 800,
             "height": 800
           }
         },
         "localized": {
-          "title": "MoscowPython",
-          "description": "Видеозаписи со встреч питонистов и джангистов в Москве и не только. :)\nПрисоединяйтесь: https://www.facebook.com/groups/MoscowDjango! :)"
+          "title": "Жекич Дубровский",
+          "description": "Основной канал сообщества \"Дубровский Синдикат\"\n\nДУБРОВСКИЙ СИНДИКАТ - крупнейшее творческое объединение, основанное в 2017 году, включающее в себя десятки популярных каналов на YouTube и других соцсетях, собственное рекламное агентство, автосалоны и сервисы проката в разных городах мира, а так же ресурсы в сфере видеоигр.\n\nПо вопросам сотрудничества, спонсорства или рекламы, писать на почту dubrovskiysyndicate@mail.ru"
         },
         "country": "RU"
       },
       "statistics": {
-        "viewCount": "2303120",
-        "subscriberCount": "25900",
+        "viewCount": "767257944",
+        "subscriberCount": "4640000",
         "hiddenSubscriberCount": false,
-        "videoCount": "685"
+        "videoCount": "360"
       }
     }
   ]
 }
+  """
 
-    """
+
+
+
--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,7 @@
+import json
+import os
+
+api_key: str = os.getenv('YT_API_KEY')
 
 
 class Channel:
@@ -5,8 +9,12 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.dict_to_print = {'kind': 'youtube#channelListResponse', 'etag': 'pYjb0XVs9dq2_Xawj-1sjUFZrM4', 'pageInfo': {'totalResults': 1, 'resultsPerPage': 5}, 'items': [{'kind': 'youtube#channel', 'etag': 'm1vtI03TiruCANlqXSqpJL4qMI4', 'id': 'UC9XJvt8OT-9_8QHDBdqocaw', 'snippet': {'title': 'Жекич Дубровский', 'description': 'Основной канал сообщества "Дубровский Синдикат"\n\nДУБРОВСКИЙ СИНДИКАТ - крупнейшее творческое объединение, основанное в 2017 году, включающее в себя десятки популярных каналов на YouTube и других соцсетях, собственное рекламное агентство, автосалоны и сервисы проката в разных городах мира, а так же ресурсы в\xa0сфере\xa0видеоигр.\n\nПо вопросам сотрудничества, спонсорства или рекламы, писать на почту dubrovskiysyndicate@mail.ru', 'customUrl': '@dubrovskiy-syndicate', 'publishedAt': '2013-03-06T02:05:58Z', 'thumbnails': {'default': {'url': 'https://yt3.ggpht.com/ytc/AGIKgqMt5pB_B63OOWd43dY5zGZAQSNczIi4rdM2Rd-DXQ=s88-c-k-c0x00ffffff-no-rj', 'width': 88, 'height': 88}, 'medium': {'url': 'https://yt3.ggpht.com/ytc/AGIKgqMt5pB_B63OOWd43dY5zGZAQSNczIi4rdM2Rd-DXQ=s240-c-k-c0x00ffffff-no-rj', 'width': 240, 'height': 240}, 'high': {'url': 'https://yt3.ggpht.com/ytc/AGIKgqMt5pB_B63OOWd43dY5zGZAQSNczIi4rdM2Rd-DXQ=s800-c-k-c0x00ffffff-no-rj', 'width': 800, 'height': 800}}, 'localized': {'title': 'Жекич Дубровский', 'description': 'Основной канал сообщества "Дубровский Синдикат"\n\nДУБРОВСКИЙ СИНДИКАТ - крупнейшее творческое объединение, основанное в 2017 году, включающее в себя десятки популярных каналов на YouTube и других соцсетях, собственное рекламное агентство, автосалоны и сервисы проката в разных городах мира, а так же ресурсы в\xa0сфере\xa0видеоигр.\n\nПо вопросам сотрудничества, спонсорства или рекламы, писать на почту dubrovskiysyndicate@mail.ru'}, 'country': 'RU'}, 'statistics': {'viewCount': '767257944', 'subscriberCount': '4640000', 'hiddenSubscriberCount': False, 'videoCount': '360'}}]}
+        self.channel_id = channel_id
 
-    def print_info(self) -> None:
+
+    def print_info(self, dict_to_print: dict) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        self.dict_to_print = dict_to_print
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+
