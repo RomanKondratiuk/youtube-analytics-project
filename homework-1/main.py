d31d857e7bfd0c16e1bc3d42530505eda0687f84
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-05-22 19:47:06 -0400

    made changes to the function

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,10 +1,10 @@
 from src.channel import Channel
 
-dictionary = {'kind': 'youtube#channelListResponse', 'etag': 'pYjb0XVs9dq2_Xawj-1sjUFZrM4', 'pageInfo': {'totalResults': 1, 'resultsPerPage': 5}, 'items': [{'kind': 'youtube#channel', 'etag': 'm1vtI03TiruCANlqXSqpJL4qMI4', 'id': 'UC9XJvt8OT-9_8QHDBdqocaw', 'snippet': {'title': 'Жекич Дубровский', 'description': 'Основной канал сообщества "Дубровский Синдикат"\n\nДУБРОВСКИЙ СИНДИКАТ - крупнейшее творческое объединение, основанное в 2017 году, включающее в себя десятки популярных каналов на YouTube и других соцсетях, собственное рекламное агентство, автосалоны и сервисы проката в разных городах мира, а так же ресурсы в\xa0сфере\xa0видеоигр.\n\nПо вопросам сотрудничества, спонсорства или рекламы, писать на почту dubrovskiysyndicate@mail.ru', 'customUrl': '@dubrovskiy-syndicate', 'publishedAt': '2013-03-06T02:05:58Z', 'thumbnails': {'default': {'url': 'https://yt3.ggpht.com/ytc/AGIKgqMt5pB_B63OOWd43dY5zGZAQSNczIi4rdM2Rd-DXQ=s88-c-k-c0x00ffffff-no-rj', 'width': 88, 'height': 88}, 'medium': {'url': 'https://yt3.ggpht.com/ytc/AGIKgqMt5pB_B63OOWd43dY5zGZAQSNczIi4rdM2Rd-DXQ=s240-c-k-c0x00ffffff-no-rj', 'width': 240, 'height': 240}, 'high': {'url': 'https://yt3.ggpht.com/ytc/AGIKgqMt5pB_B63OOWd43dY5zGZAQSNczIi4rdM2Rd-DXQ=s800-c-k-c0x00ffffff-no-rj', 'width': 800, 'height': 800}}, 'localized': {'title': 'Жекич Дубровский', 'description': 'Основной канал сообщества "Дубровский Синдикат"\n\nДУБРОВСКИЙ СИНДИКАТ - крупнейшее творческое объединение, основанное в 2017 году, включающее в себя десятки популярных каналов на YouTube и других соцсетях, собственное рекламное агентство, автосалоны и сервисы проката в разных городах мира, а так же ресурсы в\xa0сфере\xa0видеоигр.\n\nПо вопросам сотрудничества, спонсорства или рекламы, писать на почту dubrovskiysyndicate@mail.ru'}, 'country': 'RU'}, 'statistics': {'viewCount': '767257944', 'subscriberCount': '4640000', 'hiddenSubscriberCount': False, 'videoCount': '360'}}]}
+
 
 if __name__ == '__main__':
     dubrovskiy = Channel('UC9XJvt8OT-9_8QHDBdqocaw')
-    dubrovskiy.print_info(dictionary)
+    dubrovskiy.print_info()
 
     """
    {
