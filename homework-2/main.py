059cdf5e178b767724101d543efb3bba8ec9721b
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-05-29 19:40:56 -0400

    make home_work2

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,20 +1,26 @@
 from src.channel import Channel
 
 if __name__ == '__main__':
-    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+    dubrovskiy = Channel('UC9XJvt8OT-9_8QHDBdqocaw')
+    dubrovskiy.print_info()
 
     # получаем значения атрибутов
-    print(moscowpython.title)  # MoscowPython
-    print(moscowpython.video_count)  # 685 (может уже больше)
-    print(moscowpython.url)  # https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A
 
-    # менять не можем
-    moscowpython.channel_id = 'Новое название'
+    print(f'ID канала: "{dubrovskiy.id}"') # ID канала
+    print(f'название канала: "{dubrovskiy.title}"') # название канала
+    print(f'описание канала: "{dubrovskiy.description}"') # описание канала
+    print(f'ссылка на канал: "{dubrovskiy.url}"')  # ссылка на канал
+    print(f'количество подписчиков: "{dubrovskiy.subscriberCount}"')  # количество подписчиков
+    print(f'количество видео: "{dubrovskiy.video_count}"')  # количество видео
+    print(f'общее количество просмотров: "{dubrovskiy.viewCount}"')  # общее количество просмотров
+
+    #менять не можем
+    dubrovskiy._channel_id = 'Новое название'
     # AttributeError: property 'channel_id' of 'Channel' object has no setter
 
-    # можем получить объект для работы с API вне класса
+    #можем получить объект для работы с API вне класса
     print(Channel.get_service())
     # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
 
-    # создаем файл 'moscowpython.json' в данными по каналу
-    moscowpython.to_json('moscowpython.json')
+   # создаем файл 'dubrowskiy.json' в данными по каналу
+    dubrovskiy.to_json('dubrowskiy.json')
--- src/channel.py
+++ src/channel.py
@@ -3,7 +3,6 @@ import os
 from googleapiclient.discovery import build
 
 
-
 class Channel:
     """Класс для ютуб-канала"""
     # атрибуты класса
@@ -13,11 +12,46 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         # инициализатор класса
-        self.channel_id = channel_id
+        self._channel_id = channel_id
         self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        dictionary = self.channel
+        self.id = dictionary['items'][0]['id']
+        self.title = dictionary['items'][0]['snippet']['title']
+        self.description = dictionary['items'][0]['snippet']['description']
+        self.url = dictionary['items'][0]['snippet']['thumbnails']['high']['url']
+        self.subscriberCount = dictionary['items'][0]['statistics']['subscriberCount']
+        self.video_count = dictionary['items'][0]['statistics']['videoCount']
+        self.viewCount = dictionary['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    def to_json(self, file_name):
+        """метод to_json(), сохраняющий в файл значения атрибутов экземпляра Channel"""
+        data = {
+            "channel_id": self.channel_id,
+            "title": self.title,
+            "description": self.description,
+            "url": self.url,
+            "subscriberCount": self.subscriberCount,
+            "video_count": self.video_count,
+            "viewCount": self.viewCount
+        }
+        with open(file_name, "w") as json_file:
+            json.dump(data, json_file, ensure_ascii=False)
+            return data
+
+    @classmethod
+    def get_service(cls):
+        """класс-метод возвращающий объект для работы с YouTube API"""
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+    @property
+    def channel_id(self):
+        return self._channel_id
+
 
 
