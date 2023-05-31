f456de8b5853e0b34ceba6c712b4ebbc8ad911a3
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-05-30 21:38:34 -0400

    test

--- homework-3/main.py
+++ homework-3/main.py
@@ -1,18 +1,20 @@
 from src.channel import Channel
 
+dybrovskiy = dybrovskiy
+
 if __name__ == '__main__':
     # Создаем два экземпляра класса
-    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+    dubrovskiy = Channel('UC9XJvt8OT-9_8QHDBdqocaw')
     highload = Channel('UCwHL6WHUarjGfUM_586me8w')
 
     # Используем различные магические методы
-    print(moscowpython)  # 'MoscowPython (https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A)'
-    print(moscowpython + highload)  # 100100
-    print(moscowpython - highload)  # -48300
-    print(highload - moscowpython)  # 48300
-    print(moscowpython > highload)  # False
-    print(moscowpython >= highload)  # False
-    print(moscowpython < highload)  # True
-    print(moscowpython <= highload)  # True
-    print(moscowpython == highload)  # False
+    print(dybrovskiy)  # 'MoscowPython (https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A)'
+    print(dybrovskiy + highload)  # 100100
+    print(dybrovskiy - highload)  # -48300
+    print(highload - dybrovskiy)  # 48300
+    print(dybrovskiy > highload)  # False
+    print(dybrovskiy >= highload)  # False
+    print(dybrovskiy < highload)  # True
+    print(dybrovskiy <= highload)  # True
+    print(dybrovskiy == highload)  # False
     
